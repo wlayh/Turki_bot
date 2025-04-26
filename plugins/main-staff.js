08c91405635c07cfb33addb22c5949595a4d7e0e
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-04-26 14:03:47 -0600

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -12,6 +12,32 @@ let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
 > 🜸 Rol » *Creador*
 > ✧ GitHub » https://github.com/Fer280809
 
+ᰔᩚ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜
+> 🜸 Rol » *Creador*
+> ✧ GitHub » https://github.com/The-King-Destroy
+
+❒ *Colaboradores:*
+
+ᰔᩚ 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
+> 🜸 Rol » *Developer*
+> ✧ GitHub » https://github.com/Elpapiema
+
+ᰔᩚ Niño Piña
+> 🜸 Rol » *Developer*
+> ✧ GitHub » https://github.com/WillZek
+
+✧ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
+> 🜸 Rol » *Developer*
+> ✧ GitHub » https://github.com/Diomar-s
+
+ᰔᩚ I'm Fz' (Tesis)
+> 🜸 Rol » *Developer*
+> ✧ GitHub » https://github.com/FzTeis
+
+ᰔᩚ Legna
+> 🜸 Rol » *Mini-Dev* 
+> ✧ GitHub » https://github.com/Legna-chan
+
 `
 await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak)
 }
