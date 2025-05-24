a3779e92261ec9c20cf4f3334bdc5a160643d84c
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-05-24 10:23:20 -0600

    Update isla-resumen.js

--- plugins/isla-resumen.js
+++ plugins/isla-resumen.js
@@ -1,13 +1,19 @@
+import { ensureIslands } from './islas-utils.js'
+
 let handler = async (m, { conn }) => {
-    let islands = global.db.data.islands || {}
+    let islands = ensureIslands()
     let clans = global.db.data.clans || {}
     let senderId = m.sender
     let myClan = Object.keys(clans).find(c => clans[c].members.includes(senderId))
     if (!myClan) return m.reply('Debes estar en un clan.')
-    let totalMinerals = 0, totalPeople = 0, islandsOwned = []
+    let totalIron = 0, totalGold = 0, totalEmerald = 0, totalCoal = 0, totalStone = 0, totalPeople = 0, islandsOwned = []
     for (const [id, isla] of Object.entries(islands)) {
         if (isla.owner === myClan) {
-            totalMinerals += (isla.minerals || 0)
+            totalIron += (isla.iron || 0)
+            totalGold += (isla.gold || 0)
+            totalEmerald += (isla.emerald || 0)
+            totalCoal += (isla.coal || 0)
+            totalStone += (isla.stone || 0)
             totalPeople += (isla.people || 0)
             islandsOwned.push(id)
         }
@@ -15,7 +21,7 @@ let handler = async (m, { conn }) => {
     m.reply(
         `Clan: ${myClan}\n` +
         `Islas: ${islandsOwned.length} (${islandsOwned.join(', ')})\n` +
-        `Minerales: ${totalMinerals}\n` +
+        `Hierro: ${totalIron} | Oro: ${totalGold} | Esmeralda: ${totalEmerald} | Carbón: ${totalCoal} | Piedra: ${totalStone}\n` +
         `Personas/esclavos: ${totalPeople}\n` +
         `Seguidores: ${clans[myClan].seguidores || 0}`
     )
