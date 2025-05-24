74c25d3d0ee19db323d6073dd52812c6b642354f
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-05-24 10:26:25 -0600

    Update islas-utils.js

--- plugins/islas-utils.js
+++ plugins/islas-utils.js
@@ -1,4 +1,4 @@
-// Helper para asegurar que siempre hay islas creadas (280 por defecto)
+// Helper para asegurar que siempre hay 280 islas creadas y con minerales individuales
 export function ensureIslands() {
     let islands = global.db.data.islands
     if (!islands || Object.keys(islands).length < 280) {
@@ -7,7 +7,12 @@ export function ensureIslands() {
             islands[i] = {
                 owner: null,
                 name: `Isla ${i}`,
-                minerals: 100,
+                minerals: 0, // total, para compatibilidad
+                iron: 100,
+                gold: 50,
+                emerald: 10,
+                coal: 200,
+                stone: 500,
                 people: 5,
                 forSale: false,
                 price: 0,
@@ -19,6 +24,16 @@ export function ensureIslands() {
             }
         }
         global.db.data.islands = islands
+    }
+    // Asegura que cada isla tenga sus minerales individuales
+    for (let id of Object.keys(global.db.data.islands)) {
+        let isla = global.db.data.islands[id]
+        if (isla.iron === undefined) isla.iron = 100
+        if (isla.gold === undefined) isla.gold = 50
+        if (isla.emerald === undefined) isla.emerald = 10
+        if (isla.coal === undefined) isla.coal = 200
+        if (isla.stone === undefined) isla.stone = 500
+        if (isla.minerals === undefined) isla.minerals = isla.iron + isla.gold + isla.emerald + isla.coal + isla.stone
     }
     return global.db.data.islands
 }
