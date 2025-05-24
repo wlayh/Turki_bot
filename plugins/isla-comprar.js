0184fb591686ec6d8c3fc964c338c505f759f370
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-05-24 10:06:17 -0600

    Update isla-comprar.js

--- plugins/isla-comprar.js
+++ plugins/isla-comprar.js
@@ -1,19 +1,21 @@
-let handler = async (m, { conn, text, usedPrefix }) => {
-    let islands = global.db.data.islands || {}
+import { ensureIslands } from './islas-utils.js'
+
+let handler = async (m, { conn }) => {
+    let islands = ensureIslands()
     let clans = global.db.data.clans || {}
     let senderId = m.sender
     let myClan = Object.keys(clans).find(c => clans[c].members.includes(senderId))
-    let num = text.trim()
     if (!myClan) return m.reply('Debes pertenecer a un clan.')
-    if (!islands[num]) return m.reply('Isla no existe.')
-    if (islands[num].owner) return m.reply('Esa isla ya tiene dueño.')
 
-    islands[num].owner = myClan
+    // Buscar la primera isla libre
+    let libre = Object.keys(islands).find(id => !islands[id].owner)
+    if (!libre) return m.reply('No hay islas libres disponibles.')
+    islands[libre].owner = myClan
     global.db.data.islands = islands
-    m.reply(`¡La isla ${num} ahora pertenece al clan ${myClan}!`)
+    m.reply(`¡La isla ${libre} ahora pertenece al clan ${myClan}!`)
 }
 handler.tags = ['isla']
-handler.help = ['islacomprar <id_isla>']
+handler.help = ['islacomprar']
 handler.command = ['islacomprar']
 handler.group = false
 export default handler
